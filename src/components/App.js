08c3b0454c2165a8e48d5ded3b2c4488ea5e3338
Jun <junli666@gmail.com>
2022-06-18 20:21:21 +1000

    add basic router example

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+const PageOne = () => {
+  return <div>page one</div>;
+};
+
+const PageTwo = () => {
+  return <div>page two</div>;
+};
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" exact element={<PageOne />} />
+          <Route path="/pageTwo" element={<PageTwo />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
