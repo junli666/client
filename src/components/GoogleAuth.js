877562bad57d7c51e294f92dfce932ea0e9ef87f
Jun <junli666@gmail.com>
2022-06-25 21:38:57 +1000

    add callback

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function GoogleAuth() {
+  useEffect(() => {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "550429818300-dl2df9n37odmamfukthjsakdio75djgn.apps.googleusercontent.com",
+        scope: "email",
+        plugin_name: "steamy",
+      });
+    });
+  });
   return <div>GoogleAuth</div>;
 }
