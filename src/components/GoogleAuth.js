cbc7fc2298b9ac7fb703e23310607d9f7fc98b13
Jun <junli666@gmail.com>
2022-06-26 12:19:34 +1000

    display signIn status (need to use console to signin/out)

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,15 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function GoogleAuth() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
   useEffect(() => {
     window.gapi.load("client:auth2", () => {
-      window.gapi.client.init({
-        clientId:
-          "550429818300-dl2df9n37odmamfukthjsakdio75djgn.apps.googleusercontent.com",
-        scope: "email",
-        plugin_name: "steamy",
-      });
+      window.gapi.client
+        .init({
+          clientId:
+            "550429818300-dl2df9n37odmamfukthjsakdio75djgn.apps.googleusercontent.com",
+          scope: "email",
+          plugin_name: "steamy",
+        })
+        .then(() => {
+          const auth = window.gapi.auth2.getAuthInstance();
+          setIsSignedIn(auth.isSignedIn.get());
+        });
     });
   });
-  return <div>GoogleAuth</div>;
+  return (
+    <>
+      {isSignedIn && <div>signed in</div>}
+      {!isSignedIn && <div>no signed in</div>}
+    </>
+  );
 }
