4db92d41ea919fdd6e2f4d5e152add31a9a0cfe2
Jun <junli666@gmail.com>
2022-07-05 22:10:01 +1000

    bit refactor

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
+
 const INITIAL_STATE = {
   isSignedIn: null,
 };
@@ -5,9 +7,9 @@ const INITIAL_STATE = {
 export default function aaa(state = INITIAL_STATE, action) {
   console.log("in reducer -- with action", action);
   switch (action.type) {
-    case "SIGN_IN":
+    case SIGN_IN:
       return { ...state, isSignedIn: true };
-    case "SIGN_OUT":
+    case SIGN_OUT:
       return { ...state, isSignedIn: false };
     default:
       return state;
