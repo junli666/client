abdad103a23a960449c8bd481f943fb3ce1d5fbd
Jun <junli666@gmail.com>
2022-07-12 21:37:43 +1000

    basic form in redux

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { reduxForm, Field } from "redux-form";
 
-export default function StreamCreate() {
-  return <div>StreamCreate</div>;
+function StreamCreate(props) {
+  const renderInput = (formProps) => {
+    return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    );
+  };
+  return (
+    <form>
+      <Field name="title" component={renderInput} />
+      <Field name="description" component={renderInput} />
+    </form>
+  );
 }
+
+export default reduxForm({
+  form: "streamCreate",
+})(StreamCreate);
