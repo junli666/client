a98bbce53bd466f1e46a9efb9af79dec42121780
Jun <junli666@gmail.com>
2022-07-14 21:44:24 +1000

    custom form

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { reduxForm, Field } from "redux-form";
-const renderInput = ({ input }) => {
-  return <input {...input} />;
+const renderInput = ({ input, label }) => {
+  return (
+    <div className="field">
+      <label>{label}</label>
+      <input {...input} />
+    </div>
+  );
 };
 function StreamCreate(props) {
   return (
-    <form>
-      <Field name="title" component={renderInput} />
-      <Field name="description" component={renderInput} />
+    <form className="ui form">
+      <Field name="title" component={renderInput} label="Enter Title" />
+      <Field
+        name="description"
+        component={renderInput}
+        label="Enter Description"
+      />
     </form>
   );
 }
