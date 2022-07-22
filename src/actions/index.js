be47f974ce40876f583b0ab1d60115a2d184f5a2
Jun <junli666@gmail.com>
2022-07-22 21:43:03 +1000

    wire get stream

--- src/actions/index.js
+++ src/actions/index.js
@@ -22,26 +22,28 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues) => async (dispatch) => {
-  const response = streams.post("/streams", formValues);
+  const response = await streams.post("/streams", formValues);
+
   dispatch({ type: CREATE_STREAM, payload: response.data });
 };
 
 export const fetchStreams = () => async (dispatch) => {
-  const response = streams.get("/streams");
+  const response = await streams.get("/streams");
+  console.log(response.data);
   dispatch({ type: FETCH_STREAMS, payload: response.data });
 };
 
 export const fetchStream = (id) => async (dispatch) => {
-  const response = streams.get(`/streams/${id}`);
+  const response = await streams.get(`/streams/${id}`);
   dispatch({ type: FETCH_STREAM, payload: response.data });
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-  const response = streams.put(`/streams/${id}`, formValues);
+  const response = await streams.put(`/streams/${id}`, formValues);
   dispatch({ type: EDIT_STREAM, payload: response.data });
 };
 
 export const deleteStream = (id) => async (dispatch) => {
-  const response = streams.delete(`/streams/${id}`);
+  const response = await streams.delete(`/streams/${id}`);
   dispatch({ type: DELETE_STREAM, payload: response.data });
 };
--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,5 +1,13 @@
-import React from "react";
-
-export default function StreamList() {
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
+function StreamList(props) {
+  useEffect(() => {
+    console.log("useEffect start");
+    props.fetchStreams();
+    console.log("useEffect");
+  });
   return <div>StreamList</div>;
 }
+
+export default connect(null, { fetchStreams })(StreamList);
